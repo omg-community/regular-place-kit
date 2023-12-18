e3703c37e32830c1697151b42233e8ecd5000db4
iminlikewithyou <christian@omg.games>
2023-12-18 04:17:24 -0600

    create SoundUtils

---
+++ src/main/java/games/omg/utils/SoundUtils.java
@@ -0,0 +1,10 @@
+package games.omg.utils;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class SoundUtils {
+  // see https://docs.advntr.dev/sound.html
+}
