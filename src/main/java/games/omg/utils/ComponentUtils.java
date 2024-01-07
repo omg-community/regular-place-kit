bf38424c746080a2f278aca329108612d5d91f8e
iminlikewithyou <christian@omg.games>
2024-01-07 01:45:57 -0600

    create ComponentUtils (prob going to remove?)

---
+++ src/main/java/games/omg/utils/ComponentUtils.java
@@ -0,0 +1,45 @@
+package games.omg.utils;
+
+import java.util.List;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
+
+public class ComponentUtils {
+
+  // use fallback style instead in Component
+
+  // public static Component colorizeUnsetParts(Component component, TextColor color) {
+  //   if (component.style().color() == null) {
+  //       component = component.color(color);
+  //   }
+
+  //   if (component.children().isEmpty()) {
+  //       return component;
+  //   }
+
+  //   List<Component> coloredChildren = new ArrayList<>();
+  //   for (Component child : component.children()) {
+  //       coloredChildren.add(colorizeUnsetParts(child, color));
+  //   }
+
+  //   return component.children(coloredChildren);
+  // }
+
+  public static Component separateListWith(List<Component> items, Component separator) {
+    int listSize = items.size();
+
+    if (listSize == 0) return Component.empty();
+    if (listSize == 1) return items.get(0);
+
+    JoinConfiguration configuration = JoinConfiguration.builder()
+        .separator(separator)
+        .lastSeparator(separator)
+        .lastSeparatorIfSerial(separator)
+        .build();
+
+    return Component.join(configuration, items);
+  }
+
+  
+}
