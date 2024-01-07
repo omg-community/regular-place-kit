c0e6458d5875d0304dc26de1b3cd6a947b882882
iminlikewithyou <christian@omg.games>
2024-01-07 03:00:14 -0600

    send join message

--- src/main/java/games/omg/Main.java
+++ src/main/java/games/omg/Main.java
@@ -1,6 +1,11 @@
 package games.omg;
 
 import org.bukkit.plugin.java.JavaPlugin;
+
+import games.omg.utils.PlayerUtils;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,7 +14,11 @@ public class Main extends JavaPlugin implements Listener {
 	
 	@EventHandler
 	public void onLogin(PlayerJoinEvent event) {
-		
+		event.joinMessage(
+			PlayerUtils.getNametag(event.getPlayer())
+			// .colorIfAbsent(NamedTextColor.LIGHT_PURPLE)
+			.applyFallbackStyle(Style.style(NamedTextColor.LIGHT_PURPLE))
+		);
 	}
 
 	@Override
