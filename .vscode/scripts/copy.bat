0241cff1f3b79af99ecb1be23325d3349289538a
66minmin <christian@omg.games>
2024-01-14 19:55:55 -0600

    update scripts

--- .vscode/scripts/copy.bat
+++ .vscode/scripts/copy.bat
@@ -1,43 +1,36 @@
 @echo off
 setlocal
 
-REM Check if JAR_FILE variable is set
-if "%JAR_FILE%"=="" (
-  echo No JAR file specified.
-  exit /b 1
-)
-
-REM Function to check SERVER_DIRECTORY variable
-call :check_directory
-if %errorlevel% neq 0 (
-  echo Checking for .env file..
-
-  REM Check if .env file exists
-  if not exist "%ENV_FILE%" (
-    echo .env file with a defined SERVER_DIRECTORY not found at %ENV_FILE%
-    exit /b 1
-  )
-
-  REM Load environment variables from .env file
+:: If there's an env file defined, load the environment variables from it
+if exist "%ENV_FILE%" (
   for /f "delims=" %%i in (%ENV_FILE%) do set %%i
-
-  call :check_directory
-  if %errorlevel% neq 0 (
-    echo SERVER_DIRECTORY must be set in your .env file at %ENV_FILE%
-    exit /b 1
-  )
 )
 
-REM Copy JAR file to server directory
-echo Copying %JAR_FILE% to %SERVER_DIRECTORY%/plugins
-copy "%JAR_FILE%" "%SERVER_DIRECTORY%/plugins"
-
-REM End of script
-exit /b 0
-
-REM Check directory function
-:check_directory
-if "%SERVER_DIRECTORY%"=="" (
+:: Make sure a JAR file is specified
+if not exist "%JAR_FILE%" (
+  echo No JAR file specified. %JAR_FILE%
   exit /b 1
 )
-exit /b 0
+
+:: Make sure SERVER_DIRECTORY is set
+if "%SERVER_DIRECTORY%"=="" (
+  echo SERVER_DIRECTORY is not set. Please set it via an environment variable.
+  exit /b 1
+)
+
+:: Make sure SERVER_DIRECTORY exists
+if not exist "%SERVER_DIRECTORY%" (
+  echo The server directory at %SERVER_DIRECTORY% does not exist. Please create one and try again.
+  exit /b 1
+)
+
+:: Check that the server directory has a paper*.jar
+if not exist "%SERVER_DIRECTORY%\paper*.jar" (
+  echo No Paper JAR found in %SERVER_DIRECTORY%. Please place a Paper JAR in the directory and try again.
+  exit /b 1
+)
+
+:: Copy JAR file to server directory
+echo Copying %JAR_FILE% to %SERVER_DIRECTORY%/plugins
+mkdir "%SERVER_DIRECTORY%\plugins"
+copy /Y "%JAR_FILE%" "%SERVER_DIRECTORY%\plugins"
--- .vscode/scripts/launch.sh
+++ .vscode/scripts/launch.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
+# Unix-like
 
-if [ ! -f "$ENV_FILE" ]; then
-  echo ".env file not found at $ENV_FILE"
+# If there's an env file defined, load the environment variables from it
+if [ -f "$ENV_FILE" ]; then
+  export $(cat "$ENV_FILE" | xargs) >/dev/null 2>&1
+fi
+
+# Make sure SERVER_DIRECTORY is set
+if [ -z "$SERVER_DIRECTORY" ]; then
+  echo "SERVER_DIRECTORY is not set. Please set it via an environment variable."
   exit 1
 fi
 
-# Load environment variables from .env file
-export $(cat "$ENV_FILE" | xargs) >/dev/null 2>&1
+# Make sure SERVER_DIRECTORY exists
+if [ ! -d "$SERVER_DIRECTORY" ]; then
+  echo "The server directory at $SERVER_DIRECTORY does not exist. Please create one and try again."
+  exit 1
+fi
 
-# Check if SERVER_DIRECTORY is set
-if [ -z "$SERVER_DIRECTORY" ]; then
-  echo "SERVER_DIRECTORY not set in .env file."
+# Check that the server directory has a paper*.jar
+if [ ! -f "$SERVER_DIRECTORY"/paper*.jar ]; then
+  echo "No Paper JAR found in $SERVER_DIRECTORY. Please place a Paper JAR in the directory and try again."
   exit 1
 fi
 
 # Check if SERVER_ARGS is set, otherwise set it to "-Xmx1024M -Xms512M"
 if [ -z "$SERVER_ARGS" ]; then
-  echo "SERVER_ARGS not set in .env file. Setting it to \"-Xmx1024M -Xms512M\""
   export SERVER_ARGS="-Xmx1024M -Xms512M"
+  echo "Using $SERVER_ARGS as SERVER_ARGS."
 fi
 
 # Start the server
