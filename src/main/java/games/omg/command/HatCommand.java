9dfdbfec42ffd0c9ab9a166d575ac3e556f11efb
iminlikewithyou <iilwy@omg.games>
2024-01-27 21:59:10 -0600

    cleanup

--- src/main/java/games/omg/command/HatCommand.java
+++ src/main/java/games/omg/command/HatCommand.java
@@ -16,34 +16,39 @@ import net.md_5.bungee.api.ChatColor;
 public class HatCommand {
 
   public TextComponent play(CommandSender sender, String label, String[] args) {
-      Player p = (Player) sender;
-      if (args.length >= 1) return Component.text("You don't need any arguments. (/" + label.toLowerCase() + ")");
-      if (p.getInventory().getItemInMainHand().getType() == Material.AIR) return Component.text("You're not even holding anything.");
-      //if (!p.getItemInHand().getType().isBlock() && !(p.getItemInHand().getType() == Material.BANNER)) return "You can't put items on your head.";
-      ItemStack i = p.getInventory().getHelmet();
-      ItemStack i2 = p.getInventory().getItemInMainHand();
-      if (i != null && i.getType() != Material.AIR) {
-          p.getInventory().setItemInMainHand(i);
-      } else {
-          p.getInventory().setItemInMainHand(null);
-      }
-      p.getInventory().setHelmet(i2);
-      return Component.text("You are now wearing " + ChatColor.WHITE + WordUtils.capitalizeFully(i2.getType().name().replace("_", " ")) + ChatColor.RESET + ".");
+    Player p = (Player) sender;
+    if (args.length >= 1)
+      return Component.text("You don't need any arguments. (/" + label.toLowerCase() + ")");
+    if (p.getInventory().getItemInMainHand().getType() == Material.AIR)
+      return Component.text("You're not holding anything.");
+    // if (!p.getItemInHand().getType().isBlock() && !(p.getItemInHand().getType()
+    // == Material.BANNER)) return "You can't put items on your head.";
+    ItemStack i = p.getInventory().getHelmet();
+    ItemStack i2 = p.getInventory().getItemInMainHand();
+    if (i != null && i.getType() != Material.AIR) {
+      p.getInventory().setItemInMainHand(i);
+    } else {
+      p.getInventory().setItemInMainHand(null);
+    }
+    p.getInventory().setHelmet(i2);
+    // TODO: use our own stringutils for this
+    return Component.text("You are now wearing " + ChatColor.WHITE
+        + WordUtils.capitalizeFully(i2.getType().name().replace("_", " ")).replace("Tnt", "TNT") + ChatColor.RESET + ".");
   }
 
   public List<String> getAliases() {
-      return Arrays.asList("hat");
+    return Arrays.asList("hat");
   }
 
   public boolean canUse(CommandSender sender) {
-      return true;
+    return true;
   }
 
   public boolean doesSuggestedCommandHaveSpace() {
-      return false;
+    return false;
   }
 
   public TextComponent getDescription(CommandSender sender) {
-      return Component.text("Put what you're holding on your head.");
+    return Component.text("Put what you're holding on your head.");
   }
 }
