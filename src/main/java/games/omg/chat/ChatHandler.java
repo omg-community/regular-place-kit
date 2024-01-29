3f5f84cd6740cb015fb81f2f412138e18589fa9e
iminlikewithyou <iilwy@omg.games>
2024-01-28 22:56:53 -0600

    finish up the chathandler

--- src/main/java/games/omg/chat/ChatHandler.java
+++ src/main/java/games/omg/chat/ChatHandler.java
@@ -4,17 +4,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.NotNull;
 
+import games.omg.resources.ServerColors;
 import games.omg.utils.PlayerUtils;
 import io.papermc.paper.chat.ChatRenderer;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.JoinConfiguration;
-import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 
 public class ChatHandler implements ChatRenderer, Listener {
 
@@ -24,17 +24,14 @@ public class ChatHandler implements ChatRenderer, Listener {
 	 * @param event The PlayerJoinEvent
 	 */
   @EventHandler
-	public void onLogin(PlayerJoinEvent event) {
-		Component killedBy = Component.text("You were killed by ").color(NamedTextColor.RED);
+	public void onJoin(PlayerJoinEvent event) {
+		Component nametag = PlayerUtils.getNametag(event.getPlayer(), true, false).applyFallbackStyle(ServerColors.SECONDARY_COLOR);
 
-		Component nametag = PlayerUtils.getNametag(event.getPlayer());
-
-		nametag = nametag.hoverEvent(HoverEvent.showText(Component.text("This is a test hover event!")));
+		// nametag = nametag.hoverEvent(HoverEvent.showText(Component.text("This is a test hover event!")));
 
 		Component message = Component.join(
-			JoinConfiguration.noSeparators(),
-			Component.text("» ").color(NamedTextColor.GRAY),
-			killedBy,
+			JoinConfiguration.separator(Component.space()),
+			Component.text("Join").color(ServerColors.PRIMARY_COLOR),
 			nametag
 		);
 
@@ -45,8 +42,24 @@ public class ChatHandler implements ChatRenderer, Listener {
 		// );
 
 		event.getPlayer().playerListName(nametag);
-
 		event.joinMessage(message);
+	}
+
+	/**
+	 * An EventHandler method which is called when a player quits the server.
+	 * 
+	 * @param event The PlayerQuitEvent
+	 */
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		Component message = Component.join(
+			JoinConfiguration.separator(Component.space()),
+			Component.text("Quit").color(NamedTextColor.DARK_GRAY),
+			PlayerUtils.getNametag(event.getPlayer(), true, false)
+				.applyFallbackStyle(ServerColors.SECONDARY_COLOR)
+		);
+
+		event.quitMessage(message);
 	}
 
 	/**
@@ -57,13 +70,11 @@ public class ChatHandler implements ChatRenderer, Listener {
 	 */
   @Override
   public @NotNull Component render(@NotNull Player source, @NotNull Component sourceDisplayName, @NotNull Component message, @NotNull Audience audience) {
-    return Component.text("[")
-      .append(Component.text("Custom", NamedTextColor.GREEN))
-      .append(Component.text("] "))
-      .append(sourceDisplayName)
-      .append(Component.text(": "))
-      .decorate(TextDecoration.BOLD)
-      .append(message);
+    return Component.join(
+			JoinConfiguration.separator(Component.space()),
+			PlayerUtils.getNametag(source, true, true),
+			message
+		);
   }
 
 	/**
