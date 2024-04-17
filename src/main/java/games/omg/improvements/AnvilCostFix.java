e1a4f1aff38c817617e4089b59c4bf6ab2f8f06b
iminlikewithyou <iilwy@omg.games>
2024-04-16 22:55:32 -0500

    a pretty big comment and variable name overhaul

--- src/main/java/games/omg/improvements/AnvilCostFix.java
+++ src/main/java/games/omg/improvements/AnvilCostFix.java
@@ -1,9 +1,5 @@
 package games.omg.improvements;
-import java.util.logging.Logger;
-
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
@@ -11,134 +7,128 @@ import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Repairable;
-import org.bukkit.inventory.meta.ItemMeta;
 
+/**
+ * This class handles anvil repair costs, and most notably prevents anvils from displaying "Too Expensive!".
+ */
 public class AnvilCostFix implements Listener {
     
-    // the max extra levels added from a enchant is something like 13?
-    private static int max_repair_penalty = 39;
-    
+    final private static int RENAME_COST = 2;
+    final private static int REPAIR_COST = 1;
+
+    final private static int MAX_COST = 38;
+
+    /**
+     * Sets the maximum repair cost of any opened AnvilInventory to be the integer limit.
+     * 
+     * AnvilInventories only produce result items when the cost is less than their stored maximum repair cost.
+     * Setting the maximum repair cost to the integer limit will allow the game to always produce a result if it can.
+     */
     @EventHandler
     public void onAnvilOpen(InventoryOpenEvent event) {
+        if (event.getInventory() instanceof AnvilInventory) {
+            AnvilInventory inventory = ((AnvilInventory) event.getInventory());
+            inventory.setMaximumRepairCost(Integer.MAX_VALUE);
+        }
+    }
+
+    /**
+     * Sets the repair cost of the resulting item in the anvil to be, at most, the maximum repair cost.
+     * This is done to prevent any item from acquiring an internal repair cost of 39 or more, which would cause the anvil to display "Too Expensive!"
+     * and not allow the repair. This "Too Expensive!" message is client-side, and is not a server-side restriction, so it is not possible to
+     * bypass it.
+     */
+    @EventHandler
+    public void onAnvilInventoryClick(InventoryClickEvent event) {
         if (!(event.getInventory() instanceof AnvilInventory)) {
             return;
         }
 
-        AnvilInventory anvil = ((AnvilInventory) event.getInventory());
-        anvil.setMaximumRepairCost(Integer.MAX_VALUE);
-    }
+        AnvilInventory inventory = ((AnvilInventory) event.getInventory());
+        ItemStack clickedItem = event.getCurrentItem();
 
-    @EventHandler
-    public void onTakeLastItem(InventoryClickEvent event) {
-        if (!(event.getInventory() instanceof AnvilInventory)) {
+        if (clickedItem == null) {
             return;
         }
 
-        AnvilInventory anvil = ((AnvilInventory) event.getInventory());
-
-        ItemStack result = event.getCurrentItem();
-        if (event.getRawSlot() == 2 && result != null) {
-            result.setItemMeta(modifyMetaRepairCost(result, max_repair_penalty));
+        if (clickedItem == inventory.getResult()) {
+            Repairable itemMeta = (Repairable) clickedItem.getItemMeta();
+            // Note: Repair cost is stored as a 0-based value, so an item with a repair cost of 0 will actually cost 1 level to repair.
+            itemMeta.setRepairCost(clamp(itemMeta.getRepairCost(), 0, MAX_COST - 1));
+            clickedItem.setItemMeta(itemMeta);
+            event.setCurrentItem(clickedItem);
         }
     }
 
+    /**
+     * Sets the repair cost of the resulting item in the anvil to the combined repair cost of the two items.
+     */
     @EventHandler
-    public void onAnvilUse(PrepareAnvilEvent event) {
+    public void onAnvilPrepare(PrepareAnvilEvent event) {
         AnvilInventory anvil = event.getInventory();
-        ItemStack[] items = anvil.getContents();
-        
-        ItemStack item1 = items[0];
-        ItemStack item2 = items[1];
-
-        if (
-            (item1 == null || item1.getType() == Material.AIR) ||
-            (item2 == null || item2.getType() == Material.AIR)
-        ) {
-            return;
-        }
-
-        ItemMeta old_item1_meta = item1.getItemMeta();
-        ItemMeta old_item2_meta = item2.getItemMeta();
-        
-        int combined_repair_penalty = clamp(getRepairPenalty(old_item1_meta) + getRepairPenalty(old_item2_meta), 1, 39);
-
-        item1.setItemMeta(modifyMetaRepairCost(item1, combined_repair_penalty));
-        item2.setItemMeta(modifyMetaRepairCost(item2, 1));
-        
         ItemStack result = anvil.getResult();
-        
+
+        // We don't need to do anything if there's no resulting item
         if (result == null) {
-            // set the meta back to default
-            // As far as im aware, there should be no issue leaving the meta as is Technically,
-            // but it would allow people to place a item in the second slot to reset its repair penalty
-            // which kinda defeats most of the purpose of me making the costs actually matter...
-            item1.setItemMeta(old_item1_meta);
-            item2.setItemMeta(old_item2_meta);
-            
             return;
         }
-
-        if (isRepair(item1, item2, result)) {
-            // incase they take the items out, we want them to have their old meta data again
-            // so they cant just reset the penalty
-            item1.setItemMeta(old_item1_meta);
-            item2.setItemMeta(old_item2_meta);
-
-            anvil.setRepairCost(1);
-        } else if (isUpgrade(item1, item2, result)) {
-            item1.setItemMeta(old_item1_meta);
-            item2.setItemMeta(old_item2_meta);
-
-            int rep_cost = clamp(anvil.getRepairCost(), 1, max_repair_penalty);
-
-            anvil.setRepairCost(clamp(combined_repair_penalty, rep_cost, max_repair_penalty));
-        }
-
-        return;
-    }
-
-    private int clamp(int v, int min, int max) {
-        return Math.max(min, Math.min(max, v));
-    }
-
-    private ItemMeta modifyMetaRepairCost(ItemStack item, int v) {
-        Repairable i_meta = ((Repairable)item.getItemMeta());
-        i_meta.setRepairCost(clamp(getRepairPenalty(i_meta), 1, v));
         
-        return i_meta;
-    }
+        ItemStack firstItem = anvil.getFirstItem();
+        ItemStack secondItem = anvil.getSecondItem();
 
-    private int getRepairPenalty(ItemMeta meta) {
-        // Fucking Null
-        if (meta != null) {
-            return ((Repairable) meta).getRepairCost();
+        int anvilCost;
+
+        if (secondItem == null) {
+            // We're renaming, set the cost to the rename cost
+            anvilCost = RENAME_COST;
+        } else {
+            if (isRepair(firstItem, secondItem)) {
+                // We're repairing, set the cost to the repair cost
+                anvilCost = REPAIR_COST;
+            } else {
+                anvilCost = clamp(anvil.getRepairCost(), 1, MAX_COST);
+            }
         }
 
-        return 1;
+        anvil.setRepairCost(anvilCost);
     }
 
-    private boolean isRepair(ItemStack item1, ItemStack item2, ItemStack result) {
-        // if the item doesnt recieve any new enchants or upgrades to those enchants,
-        // we can just say its a repair and make it cost 1 level, this is more or less irrelevant because of mending
-        // but its nice as a qol feature for before mending is gotten, or tools taht cant have mending (bows w/ infinity)
-        if (
-            (item1.isRepairableBy(item2) && item2.getType() != item1.getType()) ||
-            // if its the same item and the enchants are the same for the result, then we know its just a repair using the same item, and a small hack to stop ench books counting
-            (item1.getType() == item2.getType() && item1.getEnchantments().equals(result.getEnchantments()) && item2.getType() != Material.ENCHANTED_BOOK)
-        ) {
+    private int clamp(int value, int min, int max) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    /**
+     * Checks if the two items could combine for a repair.
+     * 
+     * Repairs can be completed with a repairable item and a material that can repair it, such as a Diamond Pickaxe
+     * and a Diamond.
+     * 
+     * They can also be completed with two items of the same type, with one of them having no enchantments.
+     * If any of the items have enchantments, it is considered an upgrade, and this method will return false.
+     * 
+     * @param item1 The first item
+     * @param item2 The second item
+     * @return Whether or not the two items could combine for a repair
+     */
+    private boolean isRepair(ItemStack item1, ItemStack item2) {
+        // If the first item is not repairable, then this cannot be a repair
+        if (!(item1.getItemMeta() instanceof Repairable)) {
+            return false;
+        }
+        // If there is no second item, then this cannot be a repair
+        if (item2 == null) {
+            return false;
+        }
+        // Diamond Pickaxes are "repairable by" only Diamonds, isRepairableBy doesn't return true with another Diamond Pickaxe
+        if (item1.isRepairableBy(item2)) {
             return true;
         }
-
-        return false;
-    }
-
-    private boolean isUpgrade(ItemStack item1, ItemStack item2, ItemStack result) {
-        if (
-            (item1.getType() == item2.getType() && !item1.getEnchantments().equals(result.getEnchantments())) || item2.getType() == Material.ENCHANTED_BOOK
-        ) {
-            return true;
+        // Here we're checking if the first item and the second item are the same type for a repair
+        if (item1.getType() == item2.getType()) {
+            // One of the items must not have any enchantments for this to be a simple repair
+            // Otherwise, that would be an upgrade, since the enchantments would be combined
+            return item1.getEnchantments().isEmpty() || item2.getEnchantments().isEmpty();
         }
-
         return false;
     }
 }
